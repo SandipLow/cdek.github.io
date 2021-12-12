aeec1e0ffdfe2f81eda343e553b72beacb3432d9
Sandip Low <sandipbkt2002@gmail.com>
2021-12-12 22:18:53 +0530

    Page upgraded...!

--- docs/js/contact.js
+++ docs/js/contact.js
@@ -1,3 +1,6 @@
+form = document.getElementById('contact-request-form')
+
+//db initialize
 firebase.initializeApp({
     apiKey: "AIzaSyAn3eeoZ52fCepqUmgc2X7UOGFlMT8w7CI",
     authDomain: "cdek-8927886351.firebaseapp.com",
@@ -6,18 +9,11 @@ firebase.initializeApp({
 
 var db = firebase.firestore();
 
-db.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(doc.id);
-        console.log(doc.data());
-        render(doc);
-    });
-});
-
 function addData(form) {
-    db.collection("users").add({
+    db.collection("contact requests").add({
         name: form.name.value,
-        comment: form.comment.value
+        email: form.email.value,
+        message: form.subject.value
     })
     .then((docRef) => {
         console.log("Document written with ID: ", docRef.id);
@@ -27,3 +23,9 @@ function addData(form) {
         console.error("Error adding document: ", error);
     });
 }
+
+//Create Request
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addData(form);
+})
---
+++ docs/js/navbar.js
@@ -0,0 +1,5 @@
+ele = document.querySelector('#links-2');
+
+function toggle(cls) {
+    ele.classList.toggle(cls);
+}
